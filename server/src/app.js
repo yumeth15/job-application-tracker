718e0e086560b1dba3a0421068ee7c50063990a2
yumeth15 <67379412+yumeth15@users.noreply.github.com>
2026-09-16 11:53:52 +0530

    feat: set up express server and mongodb

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API is running",
+  });
+});
+
+module.exports = app;
